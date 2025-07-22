bdfccd8fc991dec3017b52440403f93fb4a564fc
S1mon <jaforsi.kozak@gmail.com>
2025-07-22 05:38:56 +0200

    implemented expr

---
+++ interpreter/expr.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+
+import error
+
+
+@dataclass
+class node:
+    kind : str
+    content : str | int
+    left  : 'node | None' = None
+    right : 'node | None' = None
+
+    def write(self):
+        pass     
+
+     
+    def eval(self):
+        pass
+
+
+
+prec_cats = (
+    ("<", ">", "<=", ">="),
+    ("+", "-"),
+    ("*"),
+    ("."),
+)
+
+
+
+def parse_expr(stream, prec_level) -> node:
+    left = parse_higher(stream, prec_level)
+
+    if str(stream.peek()) not in prec_cats[prec_level]:
+        return left
+
+    op = stream.pop()
+    right = parse_higher(stream, prec_level)
+    return node(
+        kind = 'op',
+        content = op,
+        left = left,
+        right = right
+    )
+
+#parse at higher precedence level
+def parse_higher(stream, prec_level) -> node:
+    prec_level_next = prec_level + 1
+    if prec_level_next < len(prec_cats):
+        return parse_expr(stream, prec_level_next)
+    else:
+        return parse_terminal(stream)
+
+def parse_terminal(stream) -> node:
+    match stream.pop_raw():
+        case x if x.content == '(' and x.kind == 'open_paran':
+            expr = parse_expr(stream, 0)
+            stream.expect(")")
+            return expr
+        case x if x.kind == 'numb':
+            return node(
+                kind = 'num',
+                content = int(x.content)
+            )
+        case x if x.kind == 'iden':
+            return node(
+                kind = 'var',
+                content = x.content
+            )
+        case x if x.kind == 'quote':
+            return node(
+                kind = 'string',
+                content = x.content
+            )
+        case x:
+            error.error(f"Unable to parse terminal '{x}' of type {x.kind}")
+
+
+def parse(stream) -> node:
+    return parse_expr(stream, 0)
+
